c3578d87c57760baed16a77bf97ed6cb0bb77dc0
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2022-11-22 12:13:08 +0330

    inital map fucntion

--- js/Map.js
+++ js/Map.js
@@ -1,26 +1,34 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+function setMap(){
 
+    
+    
+    
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
+    
+    
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+    
+    var circle = L.circle([51.5, -0.09], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.3,
+        radius: 200
+    }).addTo(map);
+    
+    
+    
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    circle.bindPopup("I am a circle.");
+}
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
-var circle = L.circle([51.5, -0.09], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.3,
-    radius: 200
-}).addTo(map);
-
-
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
+export default setMap;
 
 
 // var popup = L.popup()
@@ -29,13 +37,13 @@ circle.bindPopup("I am a circle.");
 //     .openOn(map);
 
 
-var popup = L.popup();
+// var popup = L.popup();
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
 
-map.on('click', onMapClick);
+// map.on('click', onMapClick);
