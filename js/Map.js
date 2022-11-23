8305cf1bb7cad45fd05f1f501d862fd6e56853ba
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2022-11-23 06:37:37 +0330

    Add geographic coordinates

--- js/Map.js
+++ js/Map.js
@@ -1,9 +1,10 @@
-function setMap(){
+function setMap(data){
 
+    const  lat = data.location.lat
+    const  lng = data.location.lng
     
     
-    
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([lat, lng], 13);
     
     
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,39 +12,19 @@ function setMap(){
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     
+    var marker = L.marker([lat, lng]).addTo(map);
     
-    
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    
-    var circle = L.circle([51.5, -0.09], {
+    var circle = L.circle([lat, lng], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.3,
-        radius: 200
+        radius: 300
     }).addTo(map);
     
     
     
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
+    marker.bindPopup(`${data.location.city}`).openPopup();
+    circle.bindPopup(`${data.location.city}`);
 }
 
 export default setMap;
-
-
-// var popup = L.popup()
-//     .setLatLng([51.513, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
-
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
