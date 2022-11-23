e9eb065676de8c64ec071bbfedbcd509d5d829dc
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2022-11-23 06:30:32 +0330

    Style

---
+++ js/setUp.js
@@ -0,0 +1,25 @@
+// import {getIP, renderIP} from "./Fetch.js";
+// import getIP from "./Fetch.js";
+import setMap from "./Map.js";
+
+
+function setUp(){
+    // renderUsers();
+    setMap();
+
+
+    // getIP()
+
+
+    // const p = renderIP();
+
+    // p.then(value => {
+    // console.log(value.location); // 👉️ "hello"
+    // }).catch(err => {
+    // console.log(err);
+    // });
+
+}
+
+
+export default setUp;
