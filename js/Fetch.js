192b29b7b23ea3635712ac83275f1af8f2a31620
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2022-11-24 10:24:08 +0330

    Fetch requst

--- js/Fetch.js
+++ js/Fetch.js
@@ -1,23 +1,27 @@
-async function getUsers() {
-    // let url = 'https://geo.ipify.org/service/account-balance?apiKey=at_Zxu07lt1jk0FD65l10JMqAPmueLTS';
-    let url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_Zxu07lt1jk0FD65l10JMqAPmueLTS&ipAddress=8.8.8.8';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
+import PageView from "./PageView.js";
+
+const ipUrl =
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_6Sn4JeMD9Mq9neDhjzFzjeEDHeLPo&ipAddress=";
+
+const domainUrl =
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_6Sn4JeMD9Mq9neDhjzFzjeEDHeLPo&domain=";
+
+export default async function getData(_input) {
+  PageView.hideAddressDetailsBox();
+  PageView.displayLoading();
+  let url1 = `${ipUrl}${_input}`;
+  let url2 = `${domainUrl}${_input}`;
+
+  try {
+    const response = await fetch(url1);
+    const response_1 = await response.json();
+    const data =
+      response_1.length > 0
+        ? response_1
+        : fetch(url2).then((response_2) => response_2.json());
+    PageView.hideLoading();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-async function renderUsers() {
-    let users = await getUsers();
-    // users.forEach(user => {
-        
-        console.log(users);
-    // });
-
-}
-
-export default renderUsers;
-
