d34cb135f408cdd9ffb77137ced64d0b252ee0d1
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2022-11-24 08:22:49 +0330

    set map

--- js/Map.js
+++ js/Map.js
@@ -1,30 +1,20 @@
-function setMap(data){
+function setMap(
+  data = { location: { lat: 35.7000815, lng: 51.3407519, city: "Tehren" } }
+) {
+  let lat = data.location.lat;
+  let lng = data.location.lng;
+  let city = data.location.city;
 
-    const  lat = data.location.lat
-    const  lng = data.location.lng
-    
-    
-    var map = L.map('map').setView([lat, lng], 13);
-    
-    
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    
-    var marker = L.marker([lat, lng]).addTo(map);
-    
-    var circle = L.circle([lat, lng], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.3,
-        radius: 300
-    }).addTo(map);
-    
-    
-    
-    marker.bindPopup(`${data.location.city}`).openPopup();
-    circle.bindPopup(`${data.location.city}`);
+  var map = L.map("map").setView([lat, lng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  var marker = L.marker([lat, lng]).addTo(map);
+  marker.bindPopup(`${city}`).openPopup();
 }
 
 export default setMap;
